e22063d5fff4c859cb27fb69cd97cfe86dd7f6ff
blacksavage <510381706@qq.com>
2018-08-20 00:30:45 +0800

    0.2

---
+++ src/redux/mini-redux.js
@@ -0,0 +1,26 @@
+export function createStore (reducer) {
+    let currentState = {}
+    let currentListeners = []
+
+    function getState () {
+        return currentState
+    }
+
+    function subscribe (listener) {
+        currentListeners.push(listener)
+    }
+
+    function dispatch (action) {
+        currentState = reducer(currentState, action)
+        currentListeners.forEach(v => v())
+        return action
+    }
+
+    dispatch({ type: '@REDUX/INIT' })
+
+    return {
+        getState,
+        subscribe,
+        dispatch
+    }
+}
